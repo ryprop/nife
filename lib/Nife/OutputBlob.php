81b77055696e6a2201cf12ccb24eb8e3aaaf7b49
Dan Stevens <stevens@earthit.com>
2016-07-11 13:58:42 -0500

    Add OutputBlob.

---
+++ lib/Nife/OutputBlob.php
@@ -0,0 +1,39 @@
+<?php
+
+/** 
+ * A blob that's implemented in terms of
+ * a callback function that just outputs everything
+ * to php://output
+ */
+class Nife_OutputBlob implements Nife_Blob
+{
+	/**
+	 * A function that, when called (with no arguments), outputs itself to php://output
+	 * (e.g. by using a lot of 'echo' statements or 'include'ing a file)
+	 */
+	protected $outputtable;
+	public function __construct( $outputtable ) {
+		$this->outputtable = $outputtable;
+	}
+	public function getLength() {
+		// No way to know without evaluating the template
+		return null;
+	}
+	public function __toString() {
+		ob_start();
+		try {
+			call_user_func($this->outputtable);
+		} catch( Exception $e ) {
+			ob_end_clean();
+			throw $e;
+		}
+		return ob_get_clean();
+	}
+	public function writeTo( $outputFunction ) {
+		if( Nife_Util::isEchoFunction($outputFunction) ) {
+			call_user_func($this->outputtable);
+		} else {
+			call_user_func($outputFunction, $this->__toString());
+		}
+	}
+}
---
+++ test/Nife/OutputBlobTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class Nife_OutputBlobTest extends PHPUnit_Framework_TestCase
+{
+	public function echoSomeStuff() {
+		echo "Some ";
+		echo "stuff\n";
+	}
+	
+	protected $blob;
+	
+	public function setUp() {
+		$this->blob = new Nife_OutputBlob(array($this,'echoSomeStuff'));
+	}
+	
+	public function testStringifyOutputBlob() {
+		$this->assertEquals("Some stuff\n", (string)$this->blob);
+	}
+
+	public function testEchoOutputBlob() {
+		ob_start();
+		$this->blob->writeTo( Nife_Util::getEchoFunction() );
+		$this->assertEquals("Some stuff\n", ob_get_clean());
+	}
+	
+	public function testWriteOutputBlob() {
+		$coll = new Nife_Collector();
+		$this->blob->writeTo( $coll );
+		$this->assertEquals( array("Some stuff\n"), $coll->getItems() );
+	}
+}
